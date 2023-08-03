7e17c1e71003c9cb2ccf7f6faa6db8c6d020dfe6
jpgbrito <139816968+jpgbrito@users.noreply.github.com>
2023-08-03 18:57:43 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,74 @@
+//mascara(this,'cpf')
+function mascara(i, t) {
+
+    var v= i.value;
+
+    if (isNaN(v[v.length -1])) {
+    i.value = v.substring (0,v.length -1)
+    return
+
+    }
+
+
+    if (t == 'data') {
+        
+        i.setAttribute ("maxlength", "10")
+        
+        if (v.length == 2 || v.length == 5) i.value += "/";
+        //02/08/2023
+        
+        
+  
+
+    }
+
+    if (t == 'cpf') {
+        
+        i.setAttribute ("maxlength", "14")
+        
+        if (v.length == 3 || v.length == 7) i.value += ".";
+        if (v.length == 11) i.value += "-";
+        //690.275.234-23
+
+
+
+    }   
+    
+    if (t == 'cep') {
+        
+        i.setAttribute ("maxlength", "9")
+        
+    
+        if (v.length == 5) i.value += "-";
+        //31110-272
+
+    }
+
+    if (t == 'tel') {
+    //if - se (31) 997349-6818 celular
+    //else - senao
+    
+    //(31)3264-7878 residencial
+        
+        if (v.length === 1) i.value = "(" + i.value;
+        if (v.length === 3) i.value += ")";
+
+        if (v[5] == 9) {
+        i.setAttribute ("maxlength", "15")
+        if (v.length === 10) i.value += "-";
+
+
+
+    } else {
+
+      i.setAttribute("maxlength, 14");
+      if (v.length === 9) i.value += "-";
+
+
+    }
+
+
+   }
+
+} 
+
